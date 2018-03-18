972e977ef77ff1938b24b9ac4e05130ec80581d0
Pierre Pronchery <khorben@defora.org>
2018-03-19 00:37:24 +0100

    Initial import
    This is directly inspired by https://wiki.osdev.org/Bare_Bones.

---
+++ src/main.c
@@ -0,0 +1,27 @@
+/* $Id$ */
+/* Copyright (c) 2018 Pierre Pronchery <khorben@defora.org> */
+/* This file is part of DeforaOS uKernel */
+
+
+/* private */
+/* prototypes */
+static int _console_init(void);
+
+
+/* functions */
+/* console_init */
+static int _console_init(void)
+{
+	/* TODO really implement */
+	return 0;
+}
+
+
+/* public */
+/* functions */
+/* main */
+int main(void)
+{
+	_console_init();
+	return 0;
+}
