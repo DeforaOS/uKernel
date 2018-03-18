8150cd7366e08a853cc61a4977ba9454f9bee684
Pierre Pronchery <khorben@defora.org>
2018-03-19 00:59:43 +0100

    Import basic test for Multiboot conformance

---
+++ tests/grub.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+
+#variables
+PROGNAME="grub.sh"
+#executables
+GRUBFILE="grub-file"
+UNAME="uname"
+
+
+#functions
+#error
+_error()
+{
+	echo "$PROGNAME: $@" 1>&2
+	return 2
+}
+
+
+#info
+_info()
+{
+	echo "$PROGNAME: $@"
+}
+
+
+#main
+case $($UNAME -m) in
+	amd64|i?86)
+		_info "Testing multiboot conformance"
+		$GRUBFILE --is-x86-multiboot ../uKernel.bin
+		res=$?
+		if [ $res -eq 127 ]; then
+			_error "Cannot test: $GRUBFILE not available (ignored)"
+			exit 0
+		elif [ $req -ne 0 ]; then
+			_error ""
+			exit $?
+		fi
+		;;
+	*)
+		;;
+esac
