9d1ba0a9787b88904aed7fe3b18077bd8d621472
Pierre Pronchery <khorben@defora.org>
2018-03-20 03:32:31 +0100

    Clear the VGA console upon initialization

--- src/drivers/console/vga.c
+++ src/drivers/console/vga.c
@@ -4,10 +4,37 @@
 
 
 
+/* FIXME provide <stdint.h> or use a cross-compiler */
+#if 0
+# include <stdint.h>
+#else
+typedef short int uint16_t;
+typedef long int size_t;
+#endif
+
+
+/* constants */
+#define VGA_COLUMNS	80
+#define VGA_ROWS	25
+
+
 /* functions */
 /* console_init */
+static void _init_clear(void);
+
 int console_init(void)
 {
-	/* TODO really implement */
+	_init_clear();
 	return 0;
+}
+
+static void _init_clear(void)
+{
+	uint16_t * buf = (uint16_t *)0xb8000;
+	size_t row;
+	size_t col;
+
+	for(row = 0; row < VGA_ROWS; row++)
+		for(col = 0; col < VGA_COLUMNS; col++)
+			buf[row * VGA_COLUMNS + col] = (' ' | (0x00 << 8));
 }
