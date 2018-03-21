008fa95b52633d43ef63e4836bff87133e0a106b
Pierre Pronchery <khorben@defora.org>
2018-03-22 00:47:45 +0100

    Import missing file

---
+++ include/unistd.h
@@ -0,0 +1,17 @@
+/* $Id$ */
+/* Copyright (c) 2018 Pierre Pronchery <khorben@defora.org> */
+/* This file is part of DeforaOS uKernel */
+
+
+
+#ifndef UKERNEL_UNISTD_H
+# define UKERNEL_UNISTD_H
+
+# include <stddef.h>
+# include <stdint.h>
+
+
+/* prototypes */
+void _exit(int status);
+
+#endif /* !UKERNEL_UNISTD_H */
