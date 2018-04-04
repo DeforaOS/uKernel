7411da160fdd6b1c3dc3ca23d285c05252d8c39a
Pierre Pronchery <khorben@defora.org>
2018-04-05 01:08:35 +0200

    Import a blunt version of <sys/types.h>

---
+++ include/sys/types.h
@@ -0,0 +1,15 @@
+/* $Id$ */
+/* Copyright (c) 2018 Pierre Pronchery <khorben@defora.org> */
+/* This file is part of DeforaOS uKernel */
+
+
+
+#ifndef UKERNEL_SYS_TYPES_H
+# define UKERNEL_SYS_TYPES_H
+
+
+/* types */
+typedef unsigned long paddr_t;
+typedef unsigned long vaddr_t;
+
+#endif /* !UKERNEL_SYS_TYPES_H */
