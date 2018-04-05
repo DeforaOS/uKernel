00d1287c607636c958d23c5ae1b8ef97ad58c220
Pierre Pronchery <khorben@defora.org>
2018-04-05 03:51:02 +0200

    Add more integer types and constants to <stdint.h>

--- include/stdint.h
+++ include/stdint.h
@@ -9,6 +9,26 @@
 
 
 /* types */
+# ifdef __INT8_TYPE__
+typedef __INT8_TYPE__ int8_t;
+# else
+typedef char int8_t;
+# endif
+# ifdef __INT16_TYPE__
+typedef __INT16_TYPE__ int16_t;
+# else
+typedef short int int16_t;
+# endif
+# ifdef __INT32_TYPE__
+typedef __INT32_TYPE__ int32_t;
+# else
+typedef int int32_t;
+# endif
+# ifdef __INT64_TYPE__
+typedef __INT64_TYPE__ int64_t;
+# else
+typedef long long int64_t;
+# endif
 # ifdef __UINT8_TYPE__
 typedef __UINT8_TYPE__ uint8_t;
 # else
@@ -24,7 +44,16 @@ typedef __UINT32_TYPE__ uint32_t;
 # else
 typedef unsigned int uint32_t;
 # endif
+# ifdef __UINT64_TYPE__
+typedef __UINT64_TYPE__ uint64_t;
+# else
+typedef unsigned long long uint64_t;
+# endif
 
 typedef unsigned long size_t;
+
+# ifndef SIZE_MAX
+#  define SIZE_MAX	ULONG_MAX
+# endif
 
 #endif /* !UKERNEL_STDINT_H */
