beff2a100769c8b37ec6ef05b5d6c92e8a466d6d
Pierre Pronchery <khorben@defora.org>
2018-04-05 03:37:19 +0200

    Report errors when missing the kernel

--- src/loader/main.c
+++ src/loader/main.c
@@ -19,6 +19,37 @@
 #endif
 
 
+/* private */
+/* prototypes */
+static void _loader_kernel(ukMultibootMod * mod);
+static void _loader_module(ukMultibootMod * mod);
+
+
+/* functions */
+/* loader_kernel */
+static void _loader_kernel(ukMultibootMod * mod)
+{
+	const char msg_loading[] = "Loading kernel: ";
+	const char msg_newline[] = "\n";
+
+	puts(msg_loading);
+	puts(mod->string);
+	puts(msg_newline);
+}
+
+
+/* loader_module */
+static void _loader_module(ukMultibootMod * mod)
+{
+	const char msg_loading[] = "Loading module: ";
+	const char msg_newline[] = "\n";
+
+	puts(msg_loading);
+	puts(mod->string);
+	puts(msg_newline);
+}
+
+
 /* public */
 /* functions */
 /* main */
@@ -30,7 +61,8 @@ int main(ukMultibootInfo * mi)
 	const char msg_cmdline[] = "Command line: ";
 	const char msg_modules[] = "Loading modules...\n";
 	size_t i;
-	const char msg_loading[] = "Loading module: ";
+	const char msg_failed2[] = "No modules provided\n";
+	const char msg_failed3[] = "No kernel provided\n";
 	const char msg_newline[] = "\n";
 	ukMultibootMod * mod;
 
@@ -49,16 +81,24 @@ int main(ukMultibootInfo * mi)
 		puts(mi->cmdline);
 		puts(msg_newline);
 	}
-	if(mi->flags & BOOT_MULTIBOOT_HEADER_HAS_MODS)
+	if(!(mi->flags & BOOT_MULTIBOOT_HEADER_HAS_MODS))
 	{
-		puts(msg_modules);
-		for(i = 0; i < mi->mods_count; i++)
-		{
-			puts(msg_loading);
-			mod = &mi->mods_addr[i];
-			puts(mod->string);
-			puts(msg_newline);
-		}
+		puts(msg_failed2);
+		return 2;
+	}
+	if(mi->mods_count == 0)
+	{
+		puts(msg_failed3);
+		return 3;
+	}
+	puts(msg_modules);
+	for(i = 0; i < mi->mods_count; i++)
+	{
+		mod = &mi->mods_addr[i];
+		if(i == 0)
+			_loader_kernel(mod);
+		else
+			_loader_module(mod);
 	}
 	return 0;
 }
