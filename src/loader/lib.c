f6d7587066fc2cff734f24458d2e2a282d87dd12
Pierre Pronchery <khorben@defora.org>
2018-04-05 19:45:23 +0200

    Generate a smaller version of libuKernel for the loader
    This will also help build the 64-bit version of the kernel.

---
+++ src/loader/lib.c
@@ -0,0 +1,17 @@
+/* $Id$ */
+/* Copyright (c) 2018 Pierre Pronchery <khorben@defora.org> */
+/* This file is part of DeforaOS uKernel */
+
+
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+#include "lib/errno.c"
+#include "lib/stdio.c"
+#include "lib/stdlib.c"
+#include "lib/string.c"
+#include "lib/ssp.c"
