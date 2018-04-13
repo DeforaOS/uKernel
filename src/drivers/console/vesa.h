81623ef3a520aa797982474b5a998c996e10d48c
Pierre Pronchery <khorben@defora.org>
2018-04-13 04:29:26 +0200

    Import missing file

---
+++ src/drivers/console/vesa.h
@@ -0,0 +1,10 @@
+/* $Id$ */
+/* Copyright (c) 2018 Pierre Pronchery <khorben@defora.org> */
+/* This file is part of DeforaOS uKernel */
+
+
+
+#ifndef UKERNEL_DRIVERS_CONSOLE_VESA_H
+# define UKERNEL_DRIVERS_CONSOLE_VESA_H
+
+#endif /* !UKERNEL_DRIVERS_CONSOLE_VESA_H */
