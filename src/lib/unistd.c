556142e0b5c2994ac496aae724a0148f5a2f8157
Pierre Pronchery <khorben@defora.org>
2018-06-07 09:56:56 -0400

    Implement a placeholder for read()

--- src/lib/unistd.c
+++ src/lib/unistd.c
@@ -21,6 +21,26 @@ char ** environ;
 
 
 /* functions */
+/* read */
+ssize_t read(int fildes, void * buf, size_t count)
+{
+	if(fildes < 0)
+	{
+		errno = EINVAL;
+		return -1;
+	}
+	if((size_t)fildes >= sizeof(_fildes) / sizeof(*_fildes))
+	{
+		errno = EBADF;
+		return -1;
+	}
+	if(count == 0)
+		return 0;
+	errno = ENOSYS;
+	return -1;
+}
+
+
 /* write */
 ssize_t write(int fildes, const void * buf, size_t count)
 {
