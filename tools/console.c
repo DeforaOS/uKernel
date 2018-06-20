fb701b279e3d782c5172b5a61cd3d4b2a9bf2d17
Pierre Pronchery <khorben@defora.org>
2018-06-20 21:50:13 +0200

    Buffer the output of the console until it is available

--- tools/console.c
+++ tools/console.c
@@ -4,6 +4,7 @@
 
 
 
+#include <stdio.h>
 #include <string.h>
 #include <errno.h>
 #include "drivers/console.h"
@@ -15,6 +16,9 @@
 /* variables */
 static ukConsole * _console = NULL;
 
+static char _console_buf[1024];
+static size_t _console_buf_cnt = 0;
+
 
 /* functions */
 /* console_init */
@@ -23,7 +27,11 @@ ukConsole * console_init(ukBus * bus, char const * name)
 	ukConsole * console = NULL;
 
 	if(strcmp(name, "stdio") == 0)
+	{
+		printf("%s%s%s\n", name, (bus != NULL) ? " at " : "",
+				(bus != NULL) ? bus->name : "");
 		console = _stdio_console_init(bus);
+	}
 	else
 	{
 		errno = ENODEV;
@@ -31,6 +39,7 @@ ukConsole * console_init(ukBus * bus, char const * name)
 	}
 	if(_console == NULL)
 		_console = console;
+	_console->print(_console, _console_buf, _console_buf_cnt);
 	return console;
 }
 
@@ -57,7 +66,15 @@ void console_clear(ukConsole * console)
 /* console_print */
 void console_print(ukConsole * console, char const * str, size_t len)
 {
-	if(console == NULL)
-		console = console_get_default();
-	console->print(console, str, len);
+	size_t s;
+
+	if(console == NULL && (console = console_get_default()) == NULL)
+	{
+		s = sizeof(_console_buf) - _console_buf_cnt;
+		s = (s > len) ? len : s;
+		strncpy(&_console_buf[_console_buf_cnt], str, s);
+		_console_buf_cnt += s;
+	}
+	else
+		console->print(console, str, len);
 }
