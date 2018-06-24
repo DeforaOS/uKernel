175f908c86bbc04b0e422c6ab2432ee42b3e69e9
Pierre Pronchery <khorben@defora.org>
2018-06-24 02:58:51 +0200

    Import an initial framework to manage interrupts

---
+++ include/kernel/intr.h
@@ -0,0 +1,15 @@
+/* $Id$ */
+/* Copyright (c) 2018 Pierre Pronchery <khorben@defora.org> */
+/* This file is part of DeforaOS uKernel */
+
+
+
+#ifndef UKERNEL_KERNEL_INTR_H
+# define UKERNEL_KERNEL_INTR_H
+
+
+/* constants */
+void intr_disable(void);
+void intr_enable(void);
+
+#endif /* !UKERNEL_KERNEL_INTR_H */
