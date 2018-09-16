b27bab4ff2528970769f056c12f6852ab01fda6c
Pierre Pronchery <khorben@defora.org>
2018-09-16 17:27:15 +0200

    Use the platform framework for tools as well

--- tools/init.c
+++ tools/init.c
@@ -4,23 +4,12 @@
 
 
 
-#include <stddef.h>
-#include "drivers/bus.h"
-#include "drivers/clock.h"
-#include "drivers/console.h"
-
-#define TOOLS_BUS	"tty"
-#define TOOLS_CLOCK	"sys"
-#define TOOLS_CONSOLE	"stdio"
+#include <kernel/platform.h>
 
 
 /* init */
 int init(void)
 {
-	ukBus * bus;
-
-	bus = bus_init(NULL, TOOLS_BUS);
-	console_init(bus, TOOLS_CONSOLE);
-	clock_init(NULL, TOOLS_CLOCK);
+	platform_init();
 	return 0;
 }
