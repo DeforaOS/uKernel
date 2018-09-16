64e9615a938db24fde7c49fd30c2cbc90c4396c4
Pierre Pronchery <khorben@defora.org>
2018-09-16 17:26:41 +0200

    Register the platform framework

---
+++ include/kernel/platform.h
@@ -0,0 +1,14 @@
+/* $Id$ */
+/* Copyright (c) 2018 Pierre Pronchery <khorben@defora.org> */
+/* This file is part of DeforaOS uKernel */
+
+
+
+#ifndef UKERNEL_KERNEL_PLATFORM_H
+# define UKERNEL_KERNEL_PLATFORM_H
+
+
+/* constants */
+void platform_init(void);
+
+#endif /* !UKERNEL_KERNEL_PLATFORM_H */
