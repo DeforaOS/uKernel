b4c3b3bf88433479fe00cc394f921a108476821d
Pierre Pronchery <khorben@defora.org>
2019-03-26 16:00:39 +0100

    Add a comment

--- tools/platform.c
+++ tools/platform.c
@@ -1,3 +1,5 @@
 /* $Id$ */
 /* Copyright (c) 2018 Pierre Pronchery <khorben@defora.org> */
 /* This file is part of DeforaOS uKernel */
+/* This file is a stub, used to include the auto-generated platform file from
+ * src/$(ARCH)/platform.c. */
