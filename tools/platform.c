eaaec21371258ff74f8741e83f4ea92babdcaaa0
Pierre Pronchery <khorben@defora.org>
2019-05-17 16:36:26 +0200

    Always generate platform.c for every platform

--- tools/platform.c
+++ tools/platform.c
@@ -2,4 +2,4 @@
 /* Copyright (c) 2018 Pierre Pronchery <khorben@defora.org> */
 /* This file is part of DeforaOS uKernel */
 /* This file is a stub, used to include the auto-generated platform file from
- * src/$(ARCH)/platform.c. */
+ * tools/arch/$(ARCH)/platform.c. */
