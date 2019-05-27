2907b8e98b3358beb502e65a362d7d31243195b4
Pierre Pronchery <khorben@defora.org>
2019-05-27 03:33:24 +0200

    Import all of libk in the native uKernel

--- tools/lib.c
+++ tools/lib.c
@@ -2,4 +2,22 @@
 /* Copyright (c) 2019 Pierre Pronchery <khorben@defora.org> */
 /* This file is part of DeforaOS uKernel */
 
-#include "../src/loader/lib.c"
+
+
+#include <sys/mman.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <errno.h>
+#include <ssp.h>
+
+#include "lib/errno.c"
+#include "lib/ssp.c"
+#include "lib/stdio.c"
+#include "lib/stdlib.c"
+#include "lib/string.c"
+#include "lib/sys/mman.c"
+#include "lib/time.c"
+#include "lib/unistd.c"
