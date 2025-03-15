6ed86832dcc11d1718f14b68d2a64e31b4df8c05
Pierre Pronchery <khorben@defora.org>
2025-03-15 13:29:34 +0100

    Update the copyright notice

--- src/drivers/display.c
+++ src/drivers/display.c
@@ -1,5 +1,5 @@
 /* $Id$ */
-/* Copyright (c) 2018 Pierre Pronchery <khorben@defora.org> */
+/* Copyright (c) 2018-2025 Pierre Pronchery <khorben@defora.org> */
 /* This file is part of DeforaOS uKernel */
 
 
